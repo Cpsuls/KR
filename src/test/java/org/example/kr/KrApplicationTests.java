7142dcc9616b42850ec9139ed22bad94c5eb8008
Kosta <prc.zrp@mail.ru>
2024-11-10 15:37:28 +0300

    "dss"

---
+++ src/test/java/org/example/kr/KrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
